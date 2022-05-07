74d9de345ab98c8fdfc3aaaf9026cd7873514ee0
MohanMadhava <102509146+MohanMadhava@users.noreply.github.com>
2022-05-07 21:01:16 +0530

    Add files via upload
    Commiting Java Activities

---
+++ Java/Activities/Car.java
@@ -0,0 +1,31 @@
+package Activities;
+
+public class Car {
+
+String color;
+String transmission;
+int make;
+int tyres;
+int doors;
+     Car()
+     {
+         tyres=4;
+         doors=4;
+     }
+    //Class Methods
+    public void displayCharacterstics(){
+        System.out.println("Color of the Car: " + color);
+        System.out.println("Make of the Car: " + make);
+        System.out.println("Transmission of the Car: " + transmission);
+        System.out.println("Number of doors on the car: " + doors);
+        System.out.println("Number of tyres on the car: " + tyres);
+    }
+    public void accelarate(){
+        System.out.println("The car is moving  Forward");
+    }
+    public void brake(){
+    System.out.println("The car has Stopped");
+    }
+
+
+}
